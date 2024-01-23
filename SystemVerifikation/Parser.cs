67f47d666c62f92682eae02e5f4397c418ee0ec3
Jean Bomhof <92791787+Jeanx23@users.noreply.github.com>
2024-01-23 19:10:21 +0100

    Projektdateien hinzufügen.

---
+++ SystemVerifikation/Parser.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemVerifikation
+{
+    public class Parser
+    {
+        public List<Wire> ParseWires(string Filepath)
+        {
+            List<Wire> Wires = new List<Wire>();
+            bool IsParsing = false;
+
+            try
+            {
+                using (StreamReader sr = new StreamReader(Filepath))
+                {
+                    string line;
+
+                    while ((line = sr.ReadLine()) != null)
+                    { 
+                        if (line.Contains("input"))
+                        {
+                            IsParsing = true;
+                            continue;
+                        }
+                        else if (line.Contains("assign"))
+                        {
+                            IsParsing = false;
+                            break;
+                        }
+                        if (IsParsing == true)
+                        {
+                            string[] words = line.Split(new char[] { ',' });
+                            foreach (string word in words)
+                            {
+                                string trimmedWord = word.Trim();
+                                if (!string.IsNullOrEmpty(trimmedWord))
+                                {
+                                    Wires.Add(new Wire { Name = trimmedWord });
+                                }
+                            }
+                        }                    
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+            return Wires;
+        }
+    }
+}
---
+++ SystemVerifikation/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemVerifikation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string Filepath = "ethernet_bench.txt";
+
+            Parser Parser = new Parser();
+            List<Wire> Wires  = Parser.ParseWires(Filepath);
+        }
+    }
+}
